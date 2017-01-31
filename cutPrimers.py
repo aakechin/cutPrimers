188b432014ace9b67a7ec4a56b15d5f59daeb499
aakechin <a.a.kechin@gmail.com>
2017-01-31 10:41:48 +0700

    Update cutPrimers.py

--- cutPrimers.py
+++ cutPrimers.py
@@ -74,7 +74,7 @@ def getErrors(s1,s2):
             muts.append(b+'/'+c)
     return(poses,muts)
 
-def trimPrimers(data):
+def trimPrimers(data):	
 	global primersR1_5,primersR1_3,primersR2_5,primersR2_3,primersFileR1_3,primersFileR2_3,primersFileR2_5,readsFileR2
 	global trimmedReadsR1,trimmedReadsR2,untrimmedReadsR1,untrimmedReadsR2
 	global allWork,maxPrimerLen,q1,q4,errNumber,primerLocBuf,readsPrimerNum,primersStatistics
